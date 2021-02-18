6196c684fb37ed1dd900daedb4b61cd04326d061
sione-tenifa <johntenifa@yahoo.com>
2021-02-18 08:42:19 -0700

    serial num working

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,24 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random 
+
+import parser
 
 class WordFinder:
-    ...
+    
+
+    def __init__(self, txt):
+
+        text = open(txt)
+        self.words = self.parse(text)
+        print(f"{len(self.words)} words read")
+
+    def parse(self, text):
+        
+
+        return [w.strip() for w in text]
+
+    def random(self):
+        
+
+        return random.choice(self.words)
