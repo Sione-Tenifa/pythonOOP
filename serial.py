b499c0dcfbcfa71d86a2fa3e21fadedb03411619
sione-tenifa <johntenifa@yahoo.com>
2021-02-18 08:03:54 -0700

    python practice

---
+++ serial.py
@@ -0,0 +1,28 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+    print("working")
+    def __init__(self, num):
+        self.num = num
+        
+    def number(self):
+        print(self.num)
